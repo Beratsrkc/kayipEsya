4368a9ed3f99c3881568a036de5ec5da0a472a3a
Beratsrkc <beratsarikoc6@gmail.com>
2025-10-04 12:22:52 +0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import userRoutes from "./routes/authRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is runing on port ${PORT}`);
+  connectDB();
+});
